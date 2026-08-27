2f8a9727886292dc96ff9e95b8470d8201fafeca
abuammar003 <ammarshah1252@gmail.com>
2026-08-27 20:43:33 +0530

    Created Server of the Nodejs & Express.js

--- BE/express-server.js
+++ BE/express-server.js
@@ -7,12 +7,15 @@ const app = Express();
 
 const users = [
 {
+    "id": 1,
     "name": "Abu Ammar",
     "age": 21    
 },{
+    "id": 2,
     "name": "Elon Musk",
     "age": 56
 }, {
+    "id": 3,
     "name": "Jeff Bezzos",
     "age": 62
 }
@@ -23,8 +26,18 @@ app.get("/", (req, res) => {
     res.send("<h1>Hello World!</h1>");
 });
 
+
+        // /user=> Routing.
 app.get("/users", (req, res) => {
     res.send(users)
+})
+
+        // /user/:id=> Nested & Dynamic Routing.
+app.get("/users/:id", (req, res)=> {
+    const { id } = req.params;
+    const findUser = users.filter(usr=> usr.id === Number(id));
+
+    res.send(findUser);
 })
 
 
