e57a99460ab0779407dafa41ae0914219a02c371
abuammar003 <ammarshah1252@gmail.com>
2026-09-09 19:24:53 +0530

    Create Data Folder for products and other Data to store here in thier own files Seperately (e.g: productsData.js => For Products Data)

---
+++ BE-with-DB/Express/Data/productsData.js
@@ -0,0 +1,12 @@
+// Here is the Data of the Products.
+
+const products = [
+    {
+        id: 1,
+        name: "Mac Book 2",
+        price: 180000,
+        category: "electronics"
+    }
+];
+
+module.exports = products;
--- BE-with-DB/Express/Routes/products.js
+++ BE-with-DB/Express/Routes/products.js
@@ -3,106 +3,29 @@
 
 const express = require("express");
 const router = express.Router();
+const {getProducts, getProductsByID, postProduct,
+     updateProduct, deleteProduct} = require("../Controllers/productController");   //CRUD Operations Written Here.
 
 router.use(express.json());
 
-const products = [
-    {
-        id: 1,
-        name: "Mac Book 2",
-        price: 180000,
-        category: "electronics"
-    }
-];
 
 
 // READ.
-router.get("/", (req, res) => {
-    res.status(200).json({
-        message: "This is Products API...",
-        products: products
-    });
-});
+router.get("/", getProducts);   //getProducts=> Comes from "../Controllers/productController.js";
+
     // READ by ID.
-router.get("/:id", (req, res) => {
-    const id = Number(req.params.id);
-
-    const findProduct = products.find((item)=> item.id === id);
-
-    if(!findProduct) {
-        return res.status(404).json({
-            message: "Product Not Found"
-        });
-    }
-
-    res.status(200).json(findProduct);
-});
+router.get("/:id", getProductsByID);
 
 
 // CREATE.
-router.post("/", (req, res) => {
-    const {name, price, category} = req.body;
-
-    const addProduct = {
-        id: Date.now(),
-        name,
-        price,
-        category
-    };
-
-    products.push(addProduct);
-
-
-    res.status(201).json({
-        message: "Created Product Successfully.",
-        product: addProduct
-    });
-
-});
+router.post("/", postProduct);
 
 
 // UPDATE.
-router.put("/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const {name, price, category} = req.body;
-
-    const findProduct = products.find((item)=> item.id === id);
-
-    if(!findProduct) {
-        return res.status(404).json({
-            message: "Product Not Found."
-        });
-    }
-
-    findProduct.name = name;
-    findProduct.price = price;
-    findProduct.category = category;
-
-    res.status(200).json({
-        message: "Updated Product Successfully.",
-        product: findProduct
-    });
-});
+router.put("/:id", updateProduct);
 
 
 // DELETE.
-router.delete("/:id", (req, res) => {
-    const id = Number(req.params.id);
-
-    const findProduct = products.findIndex((item)=> item.id === id);
-
-    if(!findProduct) {
-        return res.status(404).json({
-            message: "Product Not Fount"
-        });
-    }
-
-    const updatedProduct = products.splice(findProduct, 1);
-
-    res.status(200).json({
-        message: "Product Deleted Successfully.",
-        Deleted_Product: updatedProduct[0]
-    });
-});
+router.delete("/:id", deleteProduct);
 
 module.exports = router;     //Exporting This Server/API/Page/File.
